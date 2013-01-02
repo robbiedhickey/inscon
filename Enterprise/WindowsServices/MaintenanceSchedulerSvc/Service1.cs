613fff901ec012235efd1f7ea52911d956b19958
patrick.wimberly <patrick.wimberly@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-01-02 15:35:41 +0000

    INSCON-1  Added new solutions

---
+++ Enterprise/WindowsServices/MaintenanceSchedulerSvc/Service1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enterprise.WindowsServices.MaintenanceSchedulerSvc
+{
+    public partial class MaintenanceSchedulerService : ServiceBase
+    {
+        public MaintenanceSchedulerService()
+        {
+            InitializeComponent();
+
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource("MySource", "MyNewLog");
+            }
+
+            eventLog1.Source = "MySource";
+            eventLog1.Log = "MyNewLog";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("In OnStart");
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("In OnStop");
+        }
+    }
+}
