4799c0824d484c6d82aec7c61e50c1103b005d8b
robbie.hickey <robbie.hickey@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-01-18 20:33:29 +0000

    [INSCON-6] migrated what we have so far into the ClientDashboard project. It is somewhat integrated with twitter bootstrap now

---
+++ Enterprise/Applications/ClientDashboard/CDashboard/Controllers/ClaimsController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CDashboard.Controllers
+{
+    public class ClaimsController : Controller
+    {
+        //
+        // GET: /Claims/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
