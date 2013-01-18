6349b6c5c2c455b78538abc32d3cd8c8479f7942
robbie.hickey <robbie.hickey@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-01-18 20:58:50 +0000

    [INSCON-6] fixed media query issue where the top padding was not getting set to 0 without !important. Tried to get the BundleConfig to work but had trouble.

--- Enterprise/Applications/ClientDashboard/CDashboard/App_Start/BundleConfig.cs
+++ Enterprise/Applications/ClientDashboard/CDashboard/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace CDashboard
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/bootstrap.min.css", "~/Content/bootstrap-responsive.min.css", "~/Content/main.css"));
 
             bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
                         "~/Content/themes/base/jquery.ui.core.css",
