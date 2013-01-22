976e9e249aeb85f9727e78eb7eb69e114a59b5e7
robbie.hickey <robbie.hickey@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-01-22 17:27:39 +0000

    inscon-6 added new dashboard project based on bootstrap mvc4 nuget package

---
+++ Enterprise/Applications/ClientDashboardBootstrap/ClientDashboard/App_Start/ExampleLayoutsRouteConfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using BootstrapMvcSample.Controllers;
+using ClientDashboard.Controllers;
+using NavigationRoutes;
+
+namespace BootstrapMvcSample
+{
+    public class ExampleLayoutsRouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.MapNavigationRoute<HomeController>("Automatic Scaffolding", c => c.Index());
+
+            routes.MapNavigationRoute<ExampleLayoutsController>("Example Layouts", c => c.Starter())
+                  .AddChildRoute<ExampleLayoutsController>("Marketing", c => c.Marketing())
+                  .AddChildRoute<ExampleLayoutsController>("Fluid", c => c.Fluid())
+                  .AddChildRoute<ExampleLayoutsController>("Sign In", c => c.SignIn())
+                ;
+
+            routes.MapNavigationRoute<ClaimsController>("Hazard Claims", c => c.Index())
+                  .AddChildRoute<ClaimsController>("View Claim", c => c.ReadOnly())
+                ;
+        }
+    }
+}
---
+++ Enterprise/Applications/ClientDashboardBootstrap/ClientDashboard/Controllers/ClaimsController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BootstrapMvcSample.Controllers;
+
+namespace ClientDashboard.Controllers
+{
+    public class ClaimsController : BootstrapBaseController
+    {
+        //
+        // GET: /Claims/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ReadOnly()
+        {
+            return View();
+        }
+
+    }
+}
