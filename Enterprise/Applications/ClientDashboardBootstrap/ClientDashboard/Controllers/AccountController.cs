71c10292668e9077410dc6bdbfedaec92e6f5792
robbie.hickey <robbie.hickey@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-01-23 16:12:16 +0000

    inscon-6 added editor templates and created a shell Account controller

---
+++ Enterprise/Applications/ClientDashboardBootstrap/ClientDashboard/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Services;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientDashboard.Controllers
+{
+    public class AccountController : Controller
+    {
+        public void LogOff()
+        {
+            FederatedAuthentication.WSFederationAuthenticationModule.SignOut("/");
+        }
+    }
+}
