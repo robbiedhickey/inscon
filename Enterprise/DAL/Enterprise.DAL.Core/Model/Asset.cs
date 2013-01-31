27806a422a0fcb90370e27c705d5d40c3dff4bfe
michael.roof <michael.roof@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-01-31 16:43:27 +0000

    [inscon-2] add Asset tables and services as related.

---
+++ Enterprise/DAL/Enterprise.DAL.Core/Model/Asset.cs
@@ -0,0 +1,104 @@
+﻿// ***********************************************************************
+// Assembly         : Enterprise.DAL.Core
+// Author           : Michael Roof
+// Created          : 01-30-2013
+//
+// Last Modified By : Michael Roof
+// Last Modified On : 01-30-2013
+// ***********************************************************************
+// <copyright file="Asset.cs" company="Mortgage Specialist International, LLC">
+//     Copyright (c) Mortgage Specialist International, LLC. All rights reserved.
+// </copyright>
+// <summary></summary>
+// ***********************************************************************
+using System;
+
+namespace Enterprise.DAL.Core.Model
+{
+    /// <summary>
+    /// Class Asset
+    /// </summary>
+    public class Asset : ModelBase
+    {
+        /// <summary>
+        /// The _asset id
+        /// </summary>
+        private int _assetId;
+        /// <summary>
+        /// The _asset number
+        /// </summary>
+        private string _assetNumber;
+        /// <summary>
+        /// The _loan id
+        /// </summary>
+        private int? _loanId;
+        /// <summary>
+        /// The _organization id
+        /// </summary>
+        private int _organizationId;
+        /// <summary>
+        /// The _type id
+        /// </summary>
+        private int _typeId;
+        
+
+        /// <summary>
+        /// Gets or sets the asset id.
+        /// </summary>
+        /// <value>The asset id.</value>
+        public Int32 AssetId
+        {
+            get { return _assetId; }
+            set { SetProperty(ref _assetId, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the organization id.
+        /// </summary>
+        /// <value>The organization id.</value>
+        public Int32 OrganizationId
+        {
+            get { return _organizationId; }
+            set { SetProperty(ref _organizationId, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the type id.
+        /// </summary>
+        /// <value>The type id.</value>
+        public Int32 TypeId
+        {
+            get { return _typeId; }
+            set { SetProperty(ref _typeId, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the loan id.
+        /// </summary>
+        /// <value>The loan id.</value>
+        public Int32? LoanId
+        {
+            get { return _loanId; }
+            set { SetProperty(ref _loanId, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the asset number.
+        /// </summary>
+        /// <value>The asset number.</value>
+        public String AssetNumber
+        {
+            get { return _assetNumber; }
+            set { SetProperty(ref _assetNumber, value); }
+        }
+        
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Asset"/> class.
+        /// </summary>
+        public Asset()
+        {
+            EntityNumber = Asset_EntityId;
+        }
+    }
+}
---
+++ Enterprise/DAL/Enterprise.DAL.Core/Service/AssetService.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Enterprise.DAL.Core.Model;
+using Enterprise.DAL.Core.Types;
+using Enterprise.DAL.Framework.Data;
+
+namespace Enterprise.DAL.Core.Service
+{
+    /// <summary>
+    /// Class AssetService
+    /// </summary>
+    public class AssetService : ServiceBase<Asset>
+    {
+        /// <summary>
+        /// Builds the specified reader.
+        /// </summary>
+        /// <param name="reader">The reader.</param>
+        /// <returns>Asset.</returns>
+        public static Asset Build(ITypeReader reader)
+        {
+            var record = new Asset
+                {
+                    AssetId = reader.GetInt32("AssetId"),
+                    OrganizationId = reader.GetInt32("OrganizationId"),
+                    TypeId = reader.GetInt32("TypeId"),
+                    LoanId = reader.GetNullInt32("LoanId"),
+                    AssetNumber = reader.GetString("AssetNumber")
+                };
+
+            return record;
+        }
+
+        /// <summary>
+        /// Gets all assets.
+        /// </summary>
+        /// <returns>List{Asset}.</returns>
+        public List<Asset> GetAllAssets()
+        {
+            return QueryAll(SqlDatabase, Procedure.Asset_SelectAll, Build, CacheMinutesToExpire, IsCached);
+        }
+
+
+        /// <summary>
+        /// Gets the asset by id.
+        /// </summary>
+        /// <param name="id">The id.</param>
+        /// <returns>Asset.</returns>
+        public Asset GetAssetById(Int32 id)
+        {
+            if (IsCached)
+            {
+                Predicate<Asset> h = h2 => h2.AssetId == id;
+                return GetAllAssets().Find(h) ?? new Asset();
+            }
+
+            return Query(SqlDatabase, Procedure.Asset_SelectById, Build, id);
+        }
+
+
+        /// <summary>
+        /// Gets the asset by organization ID.
+        /// </summary>
+        /// <param name="organizationID">The organization ID.</param>
+        /// <returns>List{Asset}.</returns>
+        public List<Asset> GetAssetByOrganizationID(Int32 organizationID)
+        {
+            if (IsCached)
+            {
+                Predicate<Asset> h = h2 => h2.OrganizationId == organizationID;
+                return GetAllAssets().FindAll(h);
+            }
+
+            return QueryAll(SqlDatabase, Procedure.Asset_SelectByOrganizationId, Build, organizationID);
+        }
+
+        /// <summary>
+        /// Gets the asset by loan ID.
+        /// </summary>
+        /// <param name="loanID">The loan ID.</param>
+        /// <returns>List{Asset}.</returns>
+        public List<Asset> GetAssetByLoanID(Int32 loanID)
+        {
+            if (IsCached)
+            {
+                Predicate<Asset> h = h2 => h2.LoanId == loanID;
+                return GetAllAssets().FindAll(h);
+            }
+
+            return QueryAll(SqlDatabase, Procedure.Asset_SelectByLoanId, Build, loanID);
+        }
+    }
+}
