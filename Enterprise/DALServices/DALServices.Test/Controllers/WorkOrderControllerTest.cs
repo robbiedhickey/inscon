c15249f1d46a4f91295dcb870c412712e1bd4a19
robbie.hickey <robbie.hickey@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-02-21 17:56:35 +0000

    [INSCON-12] completed WorkOrderController test cases

--- Enterprise/DALServices/DALServices.Test/Controllers/WorkOrderControllerTest.cs
+++ Enterprise/DALServices/DALServices.Test/Controllers/WorkOrderControllerTest.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Data.SqlClient;
+using System.Linq;
 using System.Text;
 using System.Collections.Generic;
 using Enterprise.DAL.Core.Model;
@@ -68,8 +70,10 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
+            var actual = controller.GetAllWorkOrders();
+
+            Assert.AreEqual(9, actual.Count);
             Assert.IsNotNull(controller);
-            Assert.Inconclusive();
         }
 
         [TestMethod]
@@ -77,8 +81,14 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
+            var actual = controller.GetWorkOrderById(1);
+            //WorkOrderID	RequestID	AssetID	DateInserted
+            //1	1	1	2013-02-19 11:22:47.863
+            Assert.AreEqual(1, actual.RequestID);
+            Assert.AreEqual(1, actual.WorkOrderID);
+            Assert.AreEqual(1, actual.AssetID);
+            Assert.IsNotNull(actual.DateInserted);
             Assert.IsNotNull(controller);
-            Assert.Inconclusive();
         }
 
         [TestMethod]
@@ -86,8 +96,10 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
+            var actual = controller.GetWorkOrderById(100);
+
+            Assert.IsNull(actual);
             Assert.IsNotNull(controller);
-            Assert.Inconclusive();
         }
 
         [TestMethod]
@@ -95,8 +107,16 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
-            Assert.IsNotNull(controller);
-            Assert.Inconclusive();
+            var actual = controller.GetWorkOrdersByRequestId(1);
+
+            var record = actual.First();
+            //WorkOrderID	RequestID	AssetID	DateInserted
+            //1	1	1	2013-02-19 11:22:47.863
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(1, record.RequestID);
+            Assert.AreEqual(1, record.WorkOrderID);
+            Assert.AreEqual(1, record.AssetID);
+            Assert.IsNotNull(record.DateInserted);
         }
 
         [TestMethod]
@@ -104,8 +124,10 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
+            var actual = controller.GetWorkOrdersByRequestId(100);
+
+            Assert.AreEqual(0, actual.Count);
             Assert.IsNotNull(controller);
-            Assert.Inconclusive();
         }
 
         [TestMethod]
@@ -113,8 +135,16 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
-            Assert.IsNotNull(controller);
-            Assert.Inconclusive();
+            var actual = controller.GetWorkOrdersByAssetId(1);
+
+            var record = actual.First();
+            //WorkOrderID	RequestID	AssetID	DateInserted
+            //1	1	1	2013-02-19 11:22:47.863
+            Assert.AreEqual(1, actual.Count);
+            Assert.AreEqual(1, record.RequestID);
+            Assert.AreEqual(1, record.WorkOrderID);
+            Assert.AreEqual(1, record.AssetID);
+            Assert.IsNotNull(record.DateInserted);
         }
 
         [TestMethod]
@@ -122,8 +152,9 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
-            Assert.IsNotNull(controller);
-            Assert.Inconclusive();
+            var actual = controller.GetWorkOrdersByAssetId(100);
+
+            Assert.AreEqual(0, actual.Count);
         }
 
         [TestMethod]
@@ -131,8 +162,14 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
-            Assert.IsNotNull(controller);
-            Assert.Inconclusive();
+            var woToDelete = new WorkOrder {WorkOrderID = 1};
+
+            var result = controller.DeleteRecord(woToDelete);
+
+            var count = controller.GetAllWorkOrders();
+
+            Assert.AreEqual(8, count.Count);
+            Assert.IsTrue(result);
         }
 
         [TestMethod]
@@ -140,8 +177,13 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
-            Assert.IsNotNull(controller);
-            Assert.Inconclusive();
+            var woToDelete = new WorkOrder {WorkOrderID = 100};
+
+            controller.DeleteRecord(woToDelete);
+
+            var count = controller.GetAllWorkOrders();
+
+            Assert.AreEqual(9, count.Count);
         }
 
         [TestMethod]
@@ -149,17 +191,38 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
-            Assert.IsNotNull(controller);
-            Assert.Inconclusive();
+            var woToInsert = new WorkOrder
+                {
+                    DateInserted = new DateTime(2013, 2, 3),
+                    AssetID = 1,
+                    RequestID = 1
+                };
+
+            var resultId = controller.SaveRecord(woToInsert);
+
+            var result = controller.GetWorkOrderById(resultId);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(1, result.AssetID);
+            Assert.AreEqual(1, result.RequestID);
+            Assert.AreEqual(new DateTime(2013,2,3), result.DateInserted);
+
         }
 
         [TestMethod]
+        [ExpectedException(typeof(SqlException), "Should violate foreign key constraints")]
         public void InsertWorkOrderFail()
         {
             WorkOrderController controller = new WorkOrderController();
 
-            Assert.IsNotNull(controller);
-            Assert.Inconclusive();
+            var woToInsert = new WorkOrder
+                {
+                    DateInserted = new DateTime(2013, 2, 3),
+                    AssetID = 100,
+                    RequestID = 100
+                };
+
+            controller.SaveRecord(woToInsert);
         }
 
         [TestMethod]
@@ -167,17 +230,31 @@ namespace Enterprise.ApiServices.DALServices.Test.Controllers
         {
             WorkOrderController controller = new WorkOrderController();
 
-            Assert.IsNotNull(controller);
-            Assert.Inconclusive();
+            var woToUpdate = controller.GetWorkOrderById(1);
+
+            woToUpdate.AssetID = 2;
+            woToUpdate.RequestID = 2;
+
+            var resultId = controller.SaveRecord(woToUpdate);
+
+            var result = controller.GetWorkOrderById(resultId);
+
+            Assert.AreEqual(2, result.RequestID);
+            Assert.AreEqual(2, result.AssetID);
         }
 
         [TestMethod]
+        [ExpectedException(typeof(SqlException), "Should violate foreign key constraints")]
         public void UpdateWorkOrderFail()
         {
             WorkOrderController controller = new WorkOrderController();
 
-            Assert.IsNotNull(controller);
-            Assert.Inconclusive();
+            var woToUpdate = controller.GetWorkOrderById(1);
+
+            woToUpdate.AssetID = 200;
+            woToUpdate.RequestID = 200;
+
+            controller.SaveRecord(woToUpdate);
         }
     }
 }
