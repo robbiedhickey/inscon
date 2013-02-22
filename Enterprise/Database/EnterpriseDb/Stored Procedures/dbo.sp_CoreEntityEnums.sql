b7787e11d9376444882f0d0739362b2dc6fb2711
wade.jackson <wade.jackson@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-02-22 22:24:39 +0000

    [inscon-2]

--- Enterprise/Database/EnterpriseDb/Stored Procedures/dbo.sp_CoreEntityEnums.sql
+++ Enterprise/Database/EnterpriseDb/Stored Procedures/dbo.sp_CoreEntityEnums.sql
@@ -3,11 +3,10 @@ SET QUOTED_IDENTIFIER ON
 GO
 SET ANSI_NULLS ON
 GO
-
-CREATE Procedure [dbo].[sp_CoreEntityEnums]
+CREATE PROCEDURE [dbo].[sp_CoreEntityEnums]
 
 AS
 SELECT 'public const Int16 ' + Name + '_EntityId = ' + CONVERT(VARCHAR, EntityID) + ';'
-FROM   EnterpriseDb.dbo.Entity
-ORDER  BY Name 
+FROM   EnterpriseDb.common.Entity
+ORDER  BY Name
 GO
