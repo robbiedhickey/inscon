23ba95cd68450fcf63f91ceed9ea6f34d225320d
patrick.wimberly <patrick.wimberly@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-02-22 20:06:13 +0000

    INSCON-2  Added StyleCop

--- Enterprise/DALServices/DALServices/Controllers/AddressController.cs
+++ Enterprise/DALServices/DALServices/Controllers/AddressController.cs
@@ -1,58 +1,73 @@
-﻿using Enterprise.ApiServices.DALServices.Helpers;
-using Enterprise.ApiServices.DALServices.Repository;
-using Enterprise.DAL.Core.Model;
-using System;
-using System.Collections.Generic;
-using System.Net;
-using System.Web.Http;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressController.cs" company="Mortgage Specialist International">
+//   Copyright (c) Mortgage Specialist International.  All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the AddressController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Enterprise.ApiServices.DALServices.Controllers
 {
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Web.Http;
+
+    using Enterprise.ApiServices.DALServices.Helpers;
+    using Enterprise.ApiServices.DALServices.Repository;
+    using Enterprise.DAL.Core.Model;
+
+    /// <summary>
+    /// Handles all database access to the Address data.
+    /// </summary>
     public class AddressController : ApiController
     {
-        private readonly IAddressRepository _repository = new AddressRepository();
-        private readonly ExceptionHelper _exceptionHelper = new ExceptionHelper();
+        private readonly IAddressRepository repository = new AddressRepository();
+        private readonly ExceptionHelper exceptionHelper = new ExceptionHelper();
 
         [HttpGet]
         public List<Address> GetAllAddressRecords()
         {
-            return _repository.GetAllAddressRecords();
+            return repository.GetAllAddressRecords();
         }
 
         [HttpGet]
         public Address GetAddressRecordById(int id)
         {
-            if(id < 0)
+            if (id < 0)
             {
                 throw new HttpResponseException(
-                    _exceptionHelper.BuildHttpResponseMessage(HttpStatusCode.NotAcceptable,
-                                                             "The ID for the address must not be negative.",
-                                                             "Negative Value Not Allowed"));
+                    exceptionHelper.BuildHttpResponseMessage(
+                        HttpStatusCode.NotAcceptable,
+                        "The ID for the address must not be negative.",
+                        "Negative Value Not Allowed"));
             }
 
-            return _repository.GetAddressRecordById(id);
+            return repository.GetAddressRecordById(id);
         }
 
         [HttpGet]
-        public List<Address> GetRecordByParentIdAndEntityID(int parentId, Int16 entityId)
+        public List<Address> GetRecordByParentIdAndEntityId(int parentId, short entityId)
         {
             if (parentId < 0)
             {
                 throw new HttpResponseException(
-                    _exceptionHelper.BuildHttpResponseMessage(HttpStatusCode.NotAcceptable,
-                                                             "The ID for the parent record must not be negative.",
-                                                             "Negative Value Not Allowed"));
+                    exceptionHelper.BuildHttpResponseMessage(
+                        HttpStatusCode.NotAcceptable,
+                        "The ID for the parent record must not be negative.",
+                        "Negative Value Not Allowed"));
             }
             
             if (entityId < 0)
             {
                 throw new HttpResponseException(
-                    _exceptionHelper.BuildHttpResponseMessage(HttpStatusCode.NotAcceptable,
-                                                             "The ID for the entity must not be negative.",
-                                                             "Negative Value Not Allowed"));
+                    exceptionHelper.BuildHttpResponseMessage(
+                        HttpStatusCode.NotAcceptable,
+                        "The ID for the entity must not be negative.",
+                        "Negative Value Not Allowed"));
             }
 
-            return _repository.GetRecordByParentIdAndEntityID(parentId, entityId);
+            return repository.GetRecordByParentIdAndEntityID(parentId, entityId);
         }
 
         [HttpDelete]
@@ -61,26 +76,34 @@ namespace Enterprise.ApiServices.DALServices.Controllers
             if (address == null)
             {
                 throw new HttpResponseException(
-                    _exceptionHelper.BuildHttpResponseMessage(HttpStatusCode.NotAcceptable,
-                                                             "The address you are tying to delete must not be null.",
-                                                             "Null Value Not Allowed"));
+                    exceptionHelper.BuildHttpResponseMessage(
+                        HttpStatusCode.NotAcceptable,
+                        "The address you are tying to delete must not be null.",
+                        "Null Value Not Allowed"));
             }
 
-            return _repository.DeleteRecord(address);
+            return repository.DeleteRecord(address);
         }
 
+        /// <summary> Saves the passed in address to the database. </summary>
+        /// <param name="address"> The address to save. </param>
+        /// <returns> The ID value for the saved address. </returns>
+        /// <exception cref="HttpResponseException">
+        /// Throws a custom exception if the address parameter is null.
+        /// </exception>
         [HttpPost]
         public int SaveAddress(Address address)
         {
             if (address == null)
             {
                 throw new HttpResponseException(
-                    _exceptionHelper.BuildHttpResponseMessage(HttpStatusCode.NotAcceptable,
-                                                             "The address you are tying to save must not be null.",
-                                                             "Null Value Not Allowed"));
+                    exceptionHelper.BuildHttpResponseMessage(
+                        HttpStatusCode.NotAcceptable,
+                        "The address you are tying to save must not be null.",
+                        "Null Value Not Allowed"));
             }
 
-            return _repository.SaveRecord(address);
+            return repository.SaveRecord(address);
         }
     }
 }
--- Enterprise/DALServices/DALServices/Controllers/AddressLocationController.cs
+++ Enterprise/DALServices/DALServices/Controllers/AddressLocationController.cs
@@ -1,64 +1,113 @@
-﻿using Enterprise.ApiServices.DALServices.Helpers;
-using Enterprise.ApiServices.DALServices.Repository;
-using Enterprise.DAL.Core.Model;
-using System.Collections.Generic;
-using System.Net;
-using System.Web.Http;
-
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="AddressLocationController.cs" company="Mortgage Specialist International">
+//   Copyright (c) Mortgage Specialist International.  All rights reserved.
+// </copyright>
+// <summary>
+//   Defines the AddressLocationController type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace Enterprise.ApiServices.DALServices.Controllers
 {
+    using System.Collections.Generic;
+    using System.Net;
+    using System.Web.Http;
+
+    using Enterprise.ApiServices.DALServices.Helpers;
+    using Enterprise.ApiServices.DALServices.Repository;
+    using Enterprise.DAL.Core.Model;
+
+    /// <summary>
+    /// Handles all database action to the AddressLocation.
+    /// </summary>
     public class AddressLocationController : ApiController
     {
-        private readonly IAddressLocationRepository _repository = new AddressLocationRepository();
-        private readonly ExceptionHelper _exceptionHelper = new ExceptionHelper();
+        private readonly IAddressLocationRepository repository = new AddressLocationRepository();
+        private readonly ExceptionHelper exceptionHelper = new ExceptionHelper();
 
+        /// <summary>
+        /// Gets all AddressLocation records.
+        /// </summary>
+        /// <returns>A list of type AddressLocation.</returns>
         [HttpGet]
         public List<AddressLocation> GetAllAddressesLocationRecords()
         {
-            return _repository.GetAllAddressesLocationRecords();
+            return repository.GetAllAddressesLocationRecords();
         }
 
+        /// <summary>
+        /// Gets the address location by its id value.
+        /// </summary>
+        /// <param name="id">The id of the AddressLocation.</param>
+        /// <returns>
+        /// The AddressLocation that corresponds to the id parameter.
+        /// </returns>
+        /// <exception cref="System.Web.Http.HttpResponseException">
+        /// Throws a custom exception if the addressLocation parameter is negative.
+        /// </exception>
         [HttpGet]
         public AddressLocation GetAddressLocationById(int id)
         {
             if (id < 0)
             {
                 throw new HttpResponseException(
-                    _exceptionHelper.BuildHttpResponseMessage(HttpStatusCode.NotAcceptable,
-                                                             "The ID for the address location must not be negative.",
-                                                             "Negative Value Not Allowed"));
+                    exceptionHelper.BuildHttpResponseMessage(
+                        HttpStatusCode.NotAcceptable,
+                        "The ID for the address location must not be negative.",
+                        "Negative Value Not Allowed"));
             }
 
-            return _repository.GetAddressLocationById(id);
+            return repository.GetAddressLocationById(id);
         }
 
+        /// <summary>
+        /// Deletes the AddressLocation from the database.
+        /// </summary>
+        /// <param name="addressLocation">The address location to delete.</param>
+        /// <returns>
+        /// True if the delete is successful, otherwise, false is returned.
+        /// </returns>
+        /// <exception cref="System.Web.Http.HttpResponseException">
+        /// Throws a custom exception if the addressLocation parameter is null.
+        /// </exception>
         [HttpDelete]
         public bool DeleteRecord(AddressLocation addressLocation)
         {
             if (addressLocation == null)
             {
                 throw new HttpResponseException(
-                    _exceptionHelper.BuildHttpResponseMessage(HttpStatusCode.NotAcceptable,
-                                                             "The address location must not be null.",
-                                                             "Null Value Not Allowed"));
+                    exceptionHelper.BuildHttpResponseMessage(
+                        HttpStatusCode.NotAcceptable,
+                        "The address location must not be null.",
+                        "Null Value Not Allowed"));
             }
 
-            return _repository.DeleteRecord(addressLocation);
+            return repository.DeleteRecord(addressLocation);
         }
 
+        /// <summary>
+        /// Saves the passed in AddressLocation to the database.
+        /// </summary>
+        /// <param name="addressLocation">The address location to save.</param>
+        /// <returns>
+        /// The id of the saved record.
+        /// </returns>
+        /// <exception cref="System.Web.Http.HttpResponseException">
+        /// Throws a custom exception if the addressLocation parameter is null.
+        /// </exception>
         [HttpPost]
         public int SaveRecord(AddressLocation addressLocation)
         {
             if (addressLocation == null)
             {
                 throw new HttpResponseException(
-                    _exceptionHelper.BuildHttpResponseMessage(HttpStatusCode.NotAcceptable,
-                                                             "The address location must not be null.",
-                                                             "Null Value Not Allowed"));
+                    exceptionHelper.BuildHttpResponseMessage(
+                        HttpStatusCode.NotAcceptable,
+                        "The address location must not be null.",
+                        "Null Value Not Allowed"));
             }
 
-            return _repository.SaveRecord(addressLocation);
+            return repository.SaveRecord(addressLocation);
         }
     }
 }
