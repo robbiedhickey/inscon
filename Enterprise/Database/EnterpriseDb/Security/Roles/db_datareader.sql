9da5fddf684f4aecfe84c99aaafe31c8c7f7c5f3
wade.jackson <wade.jackson@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-02-22 13:54:06 +0000

    wdj - adding roles for the Developers

--- Enterprise/Database/EnterpriseDb/Security/Roles/db_datareader.sql
+++ Enterprise/Database/EnterpriseDb/Security/Roles/db_datareader.sql
@@ -1,2 +1,4 @@
+
+EXEC sp_addrolemember N'db_datareader', N'ISS\SQLDevelopers'
 EXEC sp_addrolemember N'db_datareader', N'ISS\iisuser'
 GO
--- Enterprise/Database/EnterpriseDb/Security/Roles/db_datawriter.sql
+++ Enterprise/Database/EnterpriseDb/Security/Roles/db_datawriter.sql
@@ -1,2 +1,4 @@
+
+EXEC sp_addrolemember N'db_datawriter', N'ISS\SQLDevelopers'
 EXEC sp_addrolemember N'db_datawriter', N'ISS\iisuser'
 GO
