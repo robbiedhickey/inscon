2719a341c052b97200eab4bf0e1ab5273fffc821
robbie.hickey <robbie.hickey@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-02-25 20:40:00 +0000

    [INSCON-9] mock up of the VendorDashboard

---
+++ Enterprise/Applications/VendorDashboard/VendorDashboard/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BootstrapMvcSample.Controllers;
+
+//using BootstrapMvcSample.Controllers;
+
+namespace ClientDashboard.Controllers
+{
+    public class HomeController : BootstrapBaseController
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Enterprise/Applications/VendorDashboard/VendorDashboard/Models/ClaimRecord.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+namespace Models
+{
+    public class ClaimRecord
+    {
+        public int Id { get; set; }
+        [Required]
+        public string CarrierName { get; set; }
+
+        [DataType(DataType.Url)]
+        public string CarrierSite { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ClaimStartDate { get; set; }
+
+        [DataType(DataType.Currency)]
+        public Decimal ClaimAmount { get; set; }
+    }
+
+    public static class ModelIntializer
+    {
+        public static List<ClaimRecord> CreateHomeInputModels()
+        {
+            return new List<ClaimRecord>
+                       {
+                           new ClaimRecord
+                               {
+                                   Id = 1,
+                                   CarrierSite = "http://foobar.com",
+                                   CarrierName = "asdf",
+                                   ClaimAmount = 100,
+                                   ClaimStartDate = DateTime.Now.AddYears(-1)
+                               },
+                           new ClaimRecord
+                               {
+                                   Id = 2,
+                                   CarrierSite = "http://foobar.com",
+                                   CarrierName = "fffff",
+                                   ClaimAmount = 200,
+                                   ClaimStartDate = DateTime.Now.AddYears(-2)
+                               },
+                       };
+        }
+    }
+
+    public static class ModelExtention
+    {
+        public static ClaimRecord Get(this List<ClaimRecord> models, int id)
+        {
+            return models.First(x => x.Id == id);
+        }
+    }
+}
