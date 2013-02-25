a71a8a5bc464f2a4a44d8321faa0121a37c1cf0f
robbie.hickey <robbie.hickey@83ae6f01-b592-46ea-8765-bd9ff04c9b3d>
2013-02-25 20:38:04 +0000

    [INSCON-9] checking in cleaned up clientdashboard

---
+++ Enterprise/Applications/ClientDashboard/ClientDashboard/Controllers/BootstrapBaseController.cs
@@ -0,0 +1,50 @@
+﻿using System.Web.Mvc;
+using BootstrapSupport;
+
+namespace BootstrapMvcSample.Controllers
+{
+    using System.Web;
+
+    public class BootstrapBaseController: Controller
+    {
+        public void Attention(string message)
+        {
+            TempData.Add(Alerts.ATTENTION, message);
+        }
+
+        public void Success(string message)
+        {
+            TempData.Add(Alerts.SUCCESS, message);
+        }
+
+        public void Information(string message)
+        {
+            TempData.Add(Alerts.INFORMATION, message);
+        }
+
+        public void Error(string message)
+        {
+            TempData.Add(Alerts.ERROR, message);
+        }
+
+        public void Attention(HtmlString message)
+        {
+            TempData.Add(Alerts.ATTENTION, message);
+        }
+
+        public void Success(HtmlString message)
+        {
+            TempData.Add(Alerts.SUCCESS, message);
+        }
+
+        public void Information(HtmlString message)
+        {
+            TempData.Add(Alerts.INFORMATION, message);
+        }
+
+        public void Error(HtmlString message)
+        {
+            TempData.Add(Alerts.ERROR, message);
+        }
+    }
+}
